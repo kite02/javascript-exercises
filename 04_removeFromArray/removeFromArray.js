5674de921021f2091d4e20733ea1e904ec6ac947
Jules <julescabanlit23@gmail.com>
2024-09-24 17:48:10 +0800

    Answered until SumAll

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (numbers, ...args) {
+
+    let newArray = [];
+    numbers.forEach(element => {
+        if(!args.includes(element))
+        newArray.push(element);
+    });
+    return newArray;
+    
 };
 
 // Do not edit below this line
