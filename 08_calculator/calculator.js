89e62059f86b26e24ba8b36cc784c8bb14b1d92b
Jules Cabanlit <julescabanlit23@gmail.com>
2024-10-15 17:45:55 +0800

    answered 07 and 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((product, number) => product * number, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let product=1;
+  if (number == 0 || number == 1) {
+    return 1;
+  } else if(number > 1){
+    for (i = number; i >= 1; i--) {
+      product *= i;
+    }
+  }
+  return product;
 };
 
 // Do not edit below this line
